7fc5ed8e51478b0641ce8904091cca6fb3d536be
Antony mn <58679882+Antonymn0@users.noreply.github.com>
2020-07-05 12:10:19 +0300

    added Edamam api
    Serch recipe from the edamam api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import SingleRecipe from './SingleRecipe.js'
 
-function App() {
+export default function App() {
+  
+  
+  const APP_ID = '74297ddd'
+  const APP_KEY = '72f971ad082fafc08d206a1300a1d0c2'
+ 
+
+  //initialize usteState
+  const [Recipes, setRecipes] = useState([])
+  const [searchTerm , setsearchTerm ] = useState('')
+  const [errtxt, seterrtxt] = useState('')
+  
+  //build API fetch string
+  var REQUEST = `https://api.edamam.com/search?q=${searchTerm}&app_id=${APP_ID}&app_key=${APP_KEY} `
+
+// fetch data from the API 
+  const fetchAPI = async () => {
+    if(searchTerm){
+      seterrtxt('')
+     try {
+        var responce = await fetch(REQUEST)
+        var data = await responce.json()
+        console.log('Fetching data...')
+        console.log(data.hits)
+        setRecipes(data.hits)
+     } catch (err) {
+        console.log(err)
+     } } else {
+       seterrtxt('* Please type something *')
+     }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+     
+    <div className="todo-list-app"> 
+    <div className='badgediv'>
+       <h1 className='header'> Search for recipe</h1> 
+     <div id="edamam-badge" data-color="white" className='Ebadge'> Edamam </div>
+      </div>
+       <div className='seachbar-container'> 
+       <input type='text'  
+       placeholder='Search here...' className='search-bar'
+        onChange={(e)=> setsearchTerm (e.target.value) }/>
+        <button className='search-button' onClick={fetchAPI}>Search </button>
+        
+       
+</div> <div className='typingErr'>{errtxt} </div>
 
-export default App;
+        <div className='recipeContainer'> 
+        {/* display recipes */ }
+        { Recipes.map( recipe => (
+          <div className='recipe'> 
+            <h2 className='recipeHeader'>  {recipe.recipe.label}</h2>
+            {/* display ingridients */}
+            <h3 className='ingridients'> Ingredients </h3> 
+             <ol className='ingridients-orderlist'> 
+            {recipe.recipe.ingredientLines.map( ingridient => (           
+                  <li className='listElement'> {ingridient}</li>            
+          ) ) }
+           </ol>
+          <p className='calories'> Calories: {Math.round(recipe.recipe.calories *100)/100} </p>
+        <img src={recipe.recipe.image} className='image' />
+        </div>
+     ) )  }
+</div> 
+
+    </div>
+  )
+}
---
+++ src/SingleRecipe.js
@@ -0,0 +1,11 @@
+import React, {useState, useRef, useEffect} from 'react';
+import uuidv4 from 'uuid/v4' 
+
+export default function SingleRecipe() {
+    
+    
+
+   return (
+       <h1> product page </h1>
+    )
+}
